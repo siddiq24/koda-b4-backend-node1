ae361954087d9f3798d99821eefe21b613236953
siddiq24 <siddiqrm24@gmail.com>
2025-11-25 14:58:20 +0700

    doc: initialize swagger

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./src/routers');
+const swaggerjsdoc = require('express-jsdoc-swagger');
+
+const option = {
+    info: {
+        version: '1.0.0',
+        title: 'Employee API',
+    },
+    servers: [
+        {
+            url: "http://localhost:3000"
+        }
+    ],
+    filesPattern: ['./src/**/*.js'],
+    swaggerUIPath: '/api-docs',
+    baseDir: __dirname
+}
+
+swaggerjsdoc(app)(option);
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/', router);
 
